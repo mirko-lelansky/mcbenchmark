223b8f68f3fb884e5e5f32d379f24d2bbce783b4
mlelansky <mlelansky@mail.de>
2017-09-17 23:49:06 +0200

    Add the commandline parsing with gengetopt.

--- src/mcbenchmark.c
+++ src/mcbenchmark.c
@@ -1,8 +1,21 @@
-#include <getopt.h>
-
+#ifdef HAVE_CONFIG_H
 #include "config.h"
+#endif
+
+#include <stdlib.h>
+#include "cmdline.h"
+
 
 int main(int argc, char *argv[])
 {
-    return 0;
+    struct gengetopt_args_info mcbenchmark_info;
+
+    if (cmdline_parser(argc, argv, &mcbenchmark_info) != 0)
+    {
+        cmdline_parser_free(&mcbenchmark_info);
+        exit(1);
+    }
+
+    cmdline_parser_free(&mcbenchmark_info);
+    exit(0);
 }
